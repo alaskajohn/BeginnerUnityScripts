f0d32c651b441a15484edb4d470b5e9e47c574a4
John Pile Jr <john@alaskajohn.com>
2014-07-20 12:52:51 -0400

    Update pileMessageQueue.cs

--- pileMessageQueue.cs
+++ pileMessageQueue.cs
@@ -1,56 +1,57 @@
-//John Pile Jr, 2013
-//For Use in Unity3D, tested with version 4.x
-//Demonstration of simple Message Queue
-//Requires pileMessageQueue Script
+//John Pile Jr, 2013 | www.johnpile.com
+//For use in Unity3D, tested with version 4.x
+//Demonstration of simple message queue
 
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class myMessage
+public class cMessage
 {
 	public float m_exitTime;
 	public string m_message;
+
+	public cMessage(string p_msg, float p_exitTime)
+	{
+		m_message = p_msg;
+		m_exitTime = p_exitTime;
+	}
 }
 
-public class MessageQueue : MonoBehaviour 
+public class messageQueue : MonoBehaviour 
 {
-	
-	private static List<myMessage> theQueue;
-	
-		void Start () 
-	{	
-		theQueue = new List<myMessage>();
-	}
-	
+	private static List<cMessage> m_queue = new List<cMessage>();
+	private bool m_enabled = true;
+
+	public Vector2 queueLocation = new Vector2(25, 25); //in pixels
+	public Vector2 messageSize = new Vector2(200, 25);  //in pixels
+
 	public static void addMessage(string pMessage, float pDuration)
 	{
-		myMessage tempMessage = new myMessage();
-		tempMessage.m_message = pMessage;
-		tempMessage.m_exitTime = Time.time + pDuration;
-		theQueue.Add(tempMessage);
+		cMessage tempMessage = new cMessage(pMessage, Time.time + pDuration);
+		m_queue.Add(tempMessage);
 	}
-	
-		void Update () 
+
+	void Update () 
 	{
-		for (int i = theQueue.Count - 1; i>=0; i--)
+		for (int i = m_queue.Count - 1; i>=0; i--)
 		{
-			if (Time.time > theQueue[i].m_exitTime)
-				theQueue.RemoveAt(i);
+			if (Time.time > m_queue[i].m_exitTime)
+				m_queue.RemoveAt(i);
 		}
+
+		if (Input.GetButtonDown("Fire2"))
+		    m_enabled = !m_enabled;
 	}
-	
+
 	void OnGUI()
 	{
-		int yPos=25;
-		
-		GUI.Label(new Rect(25, yPos, 200, 25), "Message Queue:");
-		yPos+=25;
-		
-		foreach (myMessage m in theQueue)
+		if (!m_enabled) return;
+		float yPos = queueLocation.y;
+		foreach (cMessage m in m_queue)
 		{
-			GUI.Label(new Rect(25, yPos, 200, 25), m.m_message);
-			yPos+=25;
+			GUI.Label(new Rect(queueLocation.x, yPos, messageSize.x, messageSize.y), m.m_message);
+			yPos+=messageSize.y;
 		}
 	}
 }
